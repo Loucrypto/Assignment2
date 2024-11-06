b755ca46fea47d2e1d9a5f2840eb150152ed95dd
Michael Louise Melchor <michaellouise.melchor@ontariotechu.net>
2024-11-06 13:11:21 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
-const express = require('express')
-const app = express()
-const port = 3000;
+var express = require('express');
+var router = express.Router();
 
-/* function helloworld (req,res,next)
-{
-res.setHeader('Content-Type', 'text/plain');
-res.end('Hello World')
-}
-app.use('/hello' , helloworld);
-app.listen(3000)
-*/
-app.get('/helloworld', (req, res) => {
-  res.send('Hello World!')
-})
-app.get('/goodbyeworld', (req, res) => {
-  res.send('Good Bye World!')
-})
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { 
+    title: 'Home', 
+  });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { 
+    title: 'Home', 
+  });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { 
+    title: 'About', 
+  });
+});
+
+/* GET products page. */
+router.get('/products', function(req, res, next) {
+  res.render('index', { 
+    title: 'Products', 
+  });
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { 
+    title: 'Service', 
+  });
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { 
+    title: 'Contact us', 
+  });
+});
+
+module.exports = router;
+
